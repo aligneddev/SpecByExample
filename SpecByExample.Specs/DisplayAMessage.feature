996a9a68bad60380a6a95d0fd30506548de0e396
Kevin Logan <logk34@hotmail.com>
2015-03-25 13:40:49 -0500

    First commit after our first meeting, simply start to a SpecFlow test

---
+++ SpecByExample.Specs/DisplayAMessage.feature
@@ -0,0 +1,9 @@
+﻿Feature: Display a Message
+	In order to notify a user
+	As a business owner
+	I want to show a message on the website
+
+Scenario: The user visits the website
+	Given the website loads
+	When A user visits the website
+	Then Hello World will be visible
---
+++ SpecByExample.Specs/DisplayAMessageSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecByExample.Specs
+{
+	[Binding]
+	public class DisplayAMessageSteps
+	{
+		private string htmlSource;
+
+		[Given(@"the website loads")]
+		public void GivenTheWebsiteLoads()
+		{
+			// assume the website is loaded
+		}
+
+		[When(@"A user visits the website")]
+		public void WhenAUserVisitsTheWebsite()
+		{
+			// hit the server and grab html
+			this.htmlSource = HtmlScraper.GetHtml("http://localhost:13181/");
+		}
+
+		[Then(@"(.*) will be visible")]
+		public void ThenWillBeVisible(string theMessage)
+		{
+			//
+			ScenarioContext.Current.Pending();
+			Assert.IsTrue(htmlSource.Contains(theMessage), "The message should have been visible");
+		}
+	}
+
+	public static class HtmlScraper
+	{
+		public static string GetHtml(string httpLocalhost)
+		{
+			return ""; //"Hello World";
+		}
+	}
+}
