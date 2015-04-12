badf5a5f12eb9babb935fba035f471fc08dea5a7
Kevin <logankd@gmail.com>
2015-04-11 21:36:53 -0500

    start for UI Selenium automation, moved project to SpecByExample.Web

---
+++ Common.SeleniumTools/BaseMappingPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.Remote;
+
+namespace Common.SeleniumTools
+{
+	/// <summary>
+	/// A possible internal NuGet package for base functionality layered on Selenium.
+	/// </summary>
+	public class BaseMappingPage
+	{
+		protected readonly RemoteWebDriver driver;
+
+		public BaseMappingPage(RemoteWebDriver driver)
+		{
+			this.driver = driver;
+		}
+	}
+}
---
+++ SpecByExample.UiAutomation/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.SeleniumTools;
+using OpenQA.Selenium.Remote;
+
+namespace SpecByExample.UiAutomation
+{
+	/// <summary>
+	/// UI Mapping for the home Page.
+	/// </summary>
+    public class HomePage : BaseMappingPage
+    {
+		public HomePage(RemoteWebDriver driver) : base(driver)
+		{
+		}
+    }
+}
